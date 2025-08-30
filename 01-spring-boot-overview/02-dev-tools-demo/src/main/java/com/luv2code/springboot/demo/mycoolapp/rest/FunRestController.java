69832d388170addc5292ede7593adae00e40e765
pukar10 <subedipukar@gmail.com>
2025-08-29 20:55:37 -0400

    expose test endpoint

--- 01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ 01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -7,26 +7,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class FunRestController {
 
     // expose "/" that return "Hello World"
-
     @GetMapping("/")
     public String sayHello() {
         return "Hello World!";
     }
 
     // expose a new endpoint for "workout"
-
     @GetMapping("/workout")
     public String getDailyWorkout() {
         return "Run a hard 5k!";
     }
 
     // expose a new endpoint for "fortune"
-
     @GetMapping("/fortune")
-    public String getDailyFortune() {
-        return "Today is your lucky day.";
-    }
+    public String getDailyFortune() { return "Today is your lucky day."; }
 
+    // expose new endpoint for "test"
+    @GetMapping("/test")
+    public String getTest() {
+        return "TEST";
+    }
 }
 
 
